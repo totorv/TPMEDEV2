c5a25ee07682e544d105c0a0af6486ee11774e27
LaBonneRousse <33541094+LaBonneRousse@users.noreply.github.com>
2017-11-23 09:00:35 +0100

    first commit

---
+++ TP2.cpp
@@ -0,0 +1,15 @@
+// TP2.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int a;
+	std::cout<<"helloworld";
+	std::cin>>a;
+	return 0;
+}
+
